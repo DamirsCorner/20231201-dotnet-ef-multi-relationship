c03483e7db198edd69d47c7e0dfe58f45d84c2cc
Damir Arh <damir.arh@gmail.com>
2023-11-26 08:44:45 +0100

    Manually configured relationships

--- EfCoreMultiRelationship/BloggingDbContext.cs
+++ EfCoreMultiRelationship/BloggingDbContext.cs
@@ -6,4 +6,22 @@ public class BloggingDbContext(DbContextOptions<BloggingDbContext> options)
 {
     public DbSet<User> Users => Set<User>();
     public DbSet<Post> Posts => Set<Post>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Post>()
+            .HasOne(post => post.Author)
+            .WithMany(user => user.AuthoredPosts);
+
+        modelBuilder.Entity<Post>()
+            .HasMany(post => post.AllowedToComment)
+            .WithMany(user => user.AllowedToComment)
+            .UsingEntity(
+                "PostUser",
+                entity => entity.HasOne(typeof(User)).WithMany().HasForeignKey("AllowedToCommentUserId"),
+                entity => entity.HasOne(typeof(Post)).WithMany().HasForeignKey("AllowedToCommentPostId"),
+                entity => entity.HasKey("AllowedToCommentUserId", "AllowedToCommentPostId"));
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
