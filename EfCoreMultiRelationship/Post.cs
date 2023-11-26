1b2cb9ce0c1f73a081b859ac4b9c104fba75c10b
Damir Arh <damir.arh@gmail.com>
2023-11-26 08:35:59 +0100

    Both relationships together don't work

--- EfCoreMultiRelationship/Post.cs
+++ EfCoreMultiRelationship/Post.cs
@@ -3,6 +3,6 @@ public class Post(string title)
 {
     public int PostId { get; set; }
     public string Title { get; set; } = title;
-    //public User? Author { get; set; }
+    public User? Author { get; set; }
     public List<User>? AllowedToComment { get; set; }
 }
--- EfCoreMultiRelationship/User.cs
+++ EfCoreMultiRelationship/User.cs
@@ -3,6 +3,6 @@ public class User(string name)
 {
     public int UserId { get; set; }
     public string Name { get; set; } = name;
-    //public List<Post>? AuthoredPosts { get; set; }
+    public List<Post>? AuthoredPosts { get; set; }
     public List<Post>? AllowedToComment { get; set; }
 }
